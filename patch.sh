cc9ae817b41ba1f2e2889fc2c559496fcc624a5a
Ông Tùng Dương <97881749+ongtungduong@users.noreply.github.com>
2024-03-12 16:30:33 +0700

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -6,12 +6,12 @@ installation_directory="/usr/lib/pritunl/lib/python3.8/site-packages/pritunl"
 handlers_subscription="https://gist.githubusercontent.com/ongtungduong/216d3d30f64f940f6813828746ab7e8b/raw/handlers.py"
 subscription="https://gist.githubusercontent.com/ongtungduong/e844e488cf6a2b085dde1ac94d893d26/raw/subscription.py"
 
-wget https://github.com/pritunl/pritunl/releases/download/${printunl_version}/pritunl_${printunl_version}-0ubuntu1.${ubuntu_codename}.deb
-dpkg -i pritunl_${printunl_version}-0ubuntu1.${ubuntu_codename}.deb
+sudo wget https://github.com/pritunl/pritunl/releases/download/${printunl_version}/pritunl_${printunl_version}-0ubuntu1.${ubuntu_codename}.deb
+sudo dpkg -i pritunl_${printunl_version}-0ubuntu1.${ubuntu_codename}.deb
 
-rm pritunl_${printunl_version}-0ubuntu1.${ubuntu_codename}.deb
+sudo rm pritunl_${printunl_version}-0ubuntu1.${ubuntu_codename}.deb
 
-curl ${handlers_subscription} > ${installation_directory}/handlers/subscription.py
-curl ${subscription} > ${installation_directory}/subscription.py
+curl ${handlers_subscription} | sudo tee ${installation_directory}/handlers/subscription.py
+curl ${subscription} | sudo tee ${installation_directory}/subscription.py
 
-systemctl restart pritunl
+sudo systemctl restart pritunl
