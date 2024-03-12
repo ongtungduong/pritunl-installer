30f71c524382b80dee04a8af4338498bc06736b9
Ông Tùng Dương <97881749+ongtungduong@users.noreply.github.com>
2024-03-12 20:58:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
 deb http://repo.pritunl.com/stable/apt jammy main
 EOF
 
+# Import signing key from keyserver
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-
+# Alternative import from download if keyserver offline
 curl https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc | sudo apt-key add -
 
 sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list << EOF
@@ -16,12 +17,10 @@ EOF
 
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
-sudo apt update
-sudo apt --assume-yes upgrade
+sudo apt update -y
 
-# Install WireGuard
 sudo apt -y install wireguard wireguard-tools
-
-# Install Pritunl and MongoDB
 sudo apt -y install pritunl mongodb-org
-sudo systemctl enable mongod pritunl --now
+
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl
